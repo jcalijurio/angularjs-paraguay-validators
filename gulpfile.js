f969198e66afebab1b1e440c53203aa3b59f5394
Jorge Calijurio <jorge.calijurio@gmail.com>
2020-04-24 21:51:33 -0300

    publish process improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var { src, dest, task, series } = require('gulp');
+const { src, dest, task, series } = require('gulp');
 const babel = require('gulp-babel');
-var concat = require('gulp-concat');
-let header = require('gulp-header');
-var uglify = require('gulp-uglify');
-var browserify = require('gulp-browserify');
+const concat = require('gulp-concat');
+const header = require('gulp-header');
+const uglify = require('gulp-uglify');
+const browserify = require('gulp-browserify');
+const cmd = require('gulp-run-command').default;
 
 const banner = `/*
  * AngularJS Paraguay Validators
@@ -15,15 +16,15 @@ const banner = `/*
 
 task('build', () => src(['./src/*.js'])
     .pipe(browserify({
-        insertGlobals: true,
+        insertGlobals: false,
         debug: false
     }))
     .pipe(babel({
         presets: ['@babel/preset-env']
     }))
     .pipe(uglify())
-    .pipe(header(banner))
     .pipe(concat('angularjs-paraguay-validator.min.js'))
+    .pipe(header(banner))
     .pipe(dest('dist/'))
 );
 
